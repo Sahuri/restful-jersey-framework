984db1cd010267818ab8b0a6d7bfea313312b666
Sahuri <huriezu21@gmail.com>
2017-07-06 11:40:04 +0700

    Add main controller

---
+++ WebContent/js/controllers/main.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['$rootScope','$scope']; 
+
+    /* @ngInject */
+    function mainController($rootScope,$scope) { 
+        
+     
+
+    }
+})();
